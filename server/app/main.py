ee759fbf43b2d43b3cbcdcc9eb44305599fe5fba
Shlok160404 <163639185+Shlok160404@users.noreply.github.com>
2025-08-25 20:53:49 +0530

    Update main.py

--- server/app/main.py
+++ server/app/main.py
@@ -1409,5 +1409,5 @@ async def get_user_dashboard(
 
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.getenv("PORT", "8000"))
+    port = int(os.getenv("PORT", "8080"))
     uvicorn.run("server.app.main:app", host="0.0.0.0", port=port, reload=True)
